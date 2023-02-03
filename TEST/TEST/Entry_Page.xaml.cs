1b0ace297565a2a60442df365a8587dca1433487
opilane <opilane@HP-CZC2349HT2>
2023-02-03 11:29:56 +0200

    Add project files.

---
+++ TEST/TEST/Entry_Page.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TEST
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Entry_Page : ContentPage 
+    { Editor ed;
+      Button btn;
+        public Entry_Page()
+        {
+            ed = new Editor
+            {
+                Placeholder = "Sisesta siia tekst",
+                BackgroundColor = Color.Pink,
+                TextColor = Color.SeaGreen
+            };
+            btn = new Button
+            {
+                Text = "Tagasi"
+            };
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children = { ed , btn },
+                BackgroundColor = Color.Purple
+            };
+            Content = st;
+            btn.Clicked += Btn_Clicked;
+        }
+
+        private async void Btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StartPage());
+        }
+    }
+}
---
+++ TEST/TEST/StartPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TEST
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            //InitializeComponent();
+            Button Entry_btn= new Button
+            {
+                Text = "Ava Entry leht",
+                TextColor = Color.Wheat,
+                BackgroundColor = Color.White
+            };
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { Entry_btn },
+                BackgroundColor = Color.YellowGreen
+            };
+            Content = st;
+            Entry_btn.Clicked += Entry_btn_Clicked;
+        }
+
+        private async void Entry_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Entry_Page());
+        }
+    }
+}
