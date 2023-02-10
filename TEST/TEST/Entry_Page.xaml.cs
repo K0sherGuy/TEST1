c891345d58a08e152c32fecacc566abc0dbf38dd
opilane <opilane@HP-CZC2349HT2>
2023-02-10 11:17:11 +0200

    BoxView, Timer

--- TEST/TEST/Entry_Page.xaml.cs
+++ TEST/TEST/Entry_Page.xaml.cs
@@ -13,6 +13,7 @@ namespace TEST
     public partial class Entry_Page : ContentPage 
     { Editor ed;
       Button btn;
+      Label lbl;
         public Entry_Page()
         {
             ed = new Editor
@@ -21,6 +22,13 @@ namespace TEST
                 BackgroundColor = Color.Pink,
                 TextColor = Color.SeaGreen
             };
+            ed.TextChanged += Ed_TextChanged;
+            lbl = new Label
+            {
+                Text="TEKST",
+                TextColor= Color.Pink,
+                BackgroundColor=Color.Orange
+            };
             btn = new Button
             {
                 Text = "Tagasi"
@@ -28,11 +36,25 @@ namespace TEST
             StackLayout st = new StackLayout
             {
                 Orientation = StackOrientation.Horizontal,
-                Children = { ed , btn },
+                Children = { ed , lbl, btn },
                 BackgroundColor = Color.Purple
             };
             Content = st;
             btn.Clicked += Btn_Clicked;
+        }
+        int i = 0;
+        private void Ed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //lbl.Text = ed.Text;
+            ed.TextChanged -= Ed_TextChanged;
+            char key = e.NewTextValue?.LastOrDefault() ?? ' ';
+
+            if (key == 'A')
+            {
+                i++;
+                lbl.Text = key.ToString() + ": " + i;
+            }
+            ed.TextChanged += Ed_TextChanged;
         }
 
         private async void Btn_Clicked(object sender, EventArgs e)
--- TEST/TEST/StartPage.xaml.cs
+++ TEST/TEST/StartPage.xaml.cs
@@ -21,14 +21,41 @@ namespace TEST
                 TextColor = Color.Wheat,
                 BackgroundColor = Color.White
             };
+
+            Button BoxView_btn = new Button
+            {
+                Text = "Ava Box leht",
+                TextColor = Color.Wheat,
+                BackgroundColor = Color.White
+            };
+
+            Button Timer_btn = new Button
+            {
+                Text = "Ava Timer leht",
+                TextColor = Color.Wheat,
+                BackgroundColor = Color.White
+            };
+
             StackLayout st = new StackLayout
             {
                 Orientation = StackOrientation.Vertical,
-                Children = { Entry_btn },
+                Children = { Entry_btn, Timer_btn, BoxView_btn },
                 BackgroundColor = Color.YellowGreen
             };
             Content = st;
             Entry_btn.Clicked += Entry_btn_Clicked;
+            Timer_btn.Clicked += Timer_btn_Clicked;
+            BoxView_btn.Clicked += BoxView_btn_Clicked;
+        }
+
+        private async void BoxView_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new BoxView_Page());
+        }
+
+        private async void Timer_btn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Timer_Page());
         }
 
         private async void Entry_btn_Clicked(object sender, EventArgs e)
